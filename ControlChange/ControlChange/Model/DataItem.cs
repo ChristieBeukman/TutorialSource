a48be10176bc91cf1e99befbc5b35a0fa15eaae5
Christie Beukman <me.mcbeukman@gmail.com>
2017-05-05 22:42:50 +0200

    Fresh

---
+++ ControlChange/ControlChange/Model/DataItem.cs
@@ -0,0 +1,16 @@
+﻿namespace ControlChange.Model
+{
+    public class DataItem
+    {
+        public string Title
+        {
+            get;
+            private set;
+        }
+
+        public DataItem(string title)
+        {
+            Title = title;
+        }
+    }
+}
